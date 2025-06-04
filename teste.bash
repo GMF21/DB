7c0a7ad2351fe2aa57f5da809f5ff0f0f4e26809
GNC <145757613+GMF21@users.noreply.github.com>
2025-06-04 10:14:26 +0000

    Update teste.bash

--- teste.bash
+++ teste.bash
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-# Lê o nome secreto sem mostrar no terminal
 read -s -p "Nome secreto: " NAME_secret
-echo ""
+echo
 
-# Ciclo infinito com for (sem limite de tentativas)
 for (( ; ; )); do
     read -p "Adivinha o nome: " guess
-
-    if [[ "$guess" == "$NAME_secret" ]]; then
-        echo "Acertaste! O nome é mesmo $NAME_secret."
+    if [ "$guess" == "$NAME_secret" ]; then
+        echo "acertaste a palavra: $NAME_secret"
         break
     else
-        echo "Errado. Tenta outra vez."
+        clear
+        echo "Errado."
     fi
 done
